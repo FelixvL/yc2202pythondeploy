f37795f5b75cfae6964e2a36555cec3ac371307e
FelixvL <digitaleservice@gmail.com>
2022-02-14 14:16:46 +0100

    wegaanmetpythonbeginnen

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    hetgetal = 25
+    if hetgetal < 20:
+        return "<p>Hello, World!"+str(hetgetal)+"</p>"
+    else:
+        return "het is groter dan 20"
+
+@app.route("/tweede/<voornaam>")
+def hello_world2(voornaam):
+    return f"<p>Hello {voornaam}, anderefunctie</p>"
